77b175cfe3b18419c623af4c32ad1bdd6fce8762
Han KOP <103375227+hankop24@users.noreply.github.com>
2023-03-15 19:08:45 +0300

    son

--- denemeProje/Assets/yürüme.cs
+++ denemeProje/Assets/yürüme.cs
@@ -2,10 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//public class yürüme : MonoBehaviour
+//{
+//    public CharacterController controller;
+//    public float speed;
+//    public float ziplamaGucu;
+//    public float gravity;
+//    Vector3 velocity;
+
+//    private void Update()
+//    {
+//        float yatay = Input.GetAxis("Horizontal");
+
+//        Vector3 direction = new Vector3(yatay, 0, 0).normalized;
+//        if (direction.magnitude >= 0.1f)
+//        {
+//            controller.Move(direction * speed * Time.deltaTime);
+//        }
+//        velocity.y += gravity * Time.deltaTime;
+//        controller.Move(velocity * Time.deltaTime);
+
+
+//        if (controller.isGrounded) // karakter yerdeyse
+//        {
+//            if (Input.GetKeyDown(KeyCode.UpArrow))
+//            {
+//                controller.Move(Vector3.up * ziplamaGucu * Time.deltaTime); // karakteri zıplat
+//            }
+//        }
+//    }
+//}
+
 public class yürüme : MonoBehaviour
 {
-
-
     Rigidbody rb;
     public float hiz;
     public float ziplamaGucu;
@@ -18,13 +47,18 @@ public class yürüme : MonoBehaviour
 
     private void Update()
     {
-        float hareket = Input.GetAxis("Horizontal") * Time.deltaTime * hiz;
-        transform.Translate(new Vector3(hareket, 0, 0)); //zıplama
+        float hareket = Input.GetAxis("Horizontal") * hiz;
+        Vector3 hareketVektoru = new Vector3(hareket, 0, 0);
+        hareketVektoru = Quaternion.Euler(0, transform.rotation.eulerAngles.y, 0) * hareketVektoru; // hareket vektörünü karakterin yönüne dönüştür
+
+        rb.velocity = new Vector3(hareketVektoru.x, rb.velocity.y, 0); // karakteri hareket ettir
+
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
             if (yerdeMi)
             {
                 rb.AddForce(Vector3.up * ziplamaGucu, ForceMode.Impulse);
+                yerdeMi = false; // karakterin havada olduğunu işaretle
             }
         }
     }
@@ -44,5 +78,55 @@ public class yürüme : MonoBehaviour
             yerdeMi = false;
         }
     }
-
 }
+
+
+
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+
+//public class yürüme : MonoBehaviour
+//{
+
+
+//    Rigidbody rb;
+//    public float hiz;
+//    public float ziplamaGucu;
+//    bool yerdeMi;
+
+//    private void Start()
+//    {
+//        rb = GetComponent<Rigidbody>();
+//    }
+
+//    private void Update()
+//    {
+//        float hareket = Input.GetAxis("Horizontal") * Time.deltaTime * hiz;
+//        transform.Translate(new Vector3(hareket, 0, 0)); //zıplama
+//        if (Input.GetKeyDown(KeyCode.UpArrow))
+//        {
+//            if (yerdeMi)
+//            {
+//                rb.AddForce(Vector3.up * ziplamaGucu, ForceMode.Impulse);
+//            }
+//        }
+//    }
+
+//    private void OnCollisionStay(Collision collision)
+//    {
+//        if (collision.gameObject.tag == "platform")
+//        {
+//            yerdeMi = true;
+//        }
+//    }
+
+//    private void OnCollisionExit(Collision collision)
+//    {
+//        if (collision.gameObject.tag == "platform")
+//        {
+//            yerdeMi = false;
+//        }
+//    }
+
+//}
