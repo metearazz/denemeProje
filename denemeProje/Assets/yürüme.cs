f991da7c8cf3ee8e6f563537a07f9922a93ba101
Han KOP <103375227+hankop24@users.noreply.github.com>
2023-03-15 16:10:08 +0300

    Revert "platform son hali"
    This reverts commit c49fae651bbb7ae751ad351bec23762cf6ba4f4b.

--- denemeProje/Assets/yürüme.cs
+++ denemeProje/Assets/yürüme.cs
@@ -24,17 +24,10 @@ public class yürüme : MonoBehaviour
         {
             if (yerdeMi)
             {
-                rb.AddForce(Vector3.up * ziplamaGucu , ForceMode.Impulse);
+                rb.AddForce(Vector3.up * ziplamaGucu, ForceMode.Impulse);
             }
         }
     }
-    //void Hareket()
-    //{
-    //    float moveX = Input.GetAxis("Horizontal"); // Get horizontal input axis value (-1 to 1)
-
-    //    Vector3 movement = new Vector3(moveX, 0f, 0f); // Create movement vector with only x-axis movement
-    //    rb.velocity = movement * hiz; // Set Rigidbody velocity to movement vector multiplied by speed
-    //}
 
     private void OnCollisionStay(Collision collision)
     {
